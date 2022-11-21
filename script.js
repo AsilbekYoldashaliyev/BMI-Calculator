ef957eb9a7402d459e43fff1714f0cc9d129ac2c
Asilbek217 <yoldashaliyev0303@gmail.com>
2022-11-21 18:35:43 +0300

    Update script.js
    ..

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // const favorite = (prompt('My favourite fruit?'));
 
 // if (favorite === apple) {
@@ -12,8 +10,32 @@
 //   `);
 // }
 
-const uzoqYolgaAvtomobileKerak = true;
-const avtomobilegaYonolgiKerak = false;
-// console.log (uzoqYolgaAvtomobileKerak && avtomobilegaYonolgiKerak);
-// console.log (uzoqYolgaAvtomobileKerak || avtomobilegaYonolgiKerak);
-const 
+// const uzoqYolgaAvtomobileKerak = true;
+// const avtomobilegaYonolgiKerak = false;
+// // console.log (uzoqYolgaAvtomobileKerak && avtomobilegaYonolgiKerak);
+// // console.log (uzoqYolgaAvtomobileKerak || avtomobilegaYonolgiKerak);
+// const
+
+let dolphinsScore1 = 96;
+
+let dolphinsScore2 = 108;
+
+let dolphinsScore3 = 89;
+
+let koalasScore1 = 88;
+
+let koalasScore2 = 91;
+
+let koalasScore3 = 110;
+
+const dolphins = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
+
+const koalas = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
+
+// console.log(dolphins)
+
+// console.log(koalas)
+
+console.log (Math.floor(dolphins))
+
+console.log (Math.floor(koalas))
