d9681af56ad602c71599ac44682bec720ea9ff1d
Asilbek217 <yoldashaliyev0303@gmail.com>
2022-11-21 17:31:48 +0300

    Starter
    ..

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// const favorite = (prompt('My favourite fruit?'));
+
+// if (favorite === apple) {
+//   console.log('apple is my favorite favourite');
+// } else if (favorite === 327) {
+//   console.log('pineapple is also my favorite number');
+// } else {
+//   console.log(
+//     `Bu meva mening yoqtirgan mevam emas. Kiritgan mevangiz: ${favorite}
+//   `);
+// }
+
+const uzoqYolgaAvtomobileKerak = true;
+const avtomobilegaYonolgiKerak = false;
+// console.log (uzoqYolgaAvtomobileKerak && avtomobilegaYonolgiKerak);
+// console.log (uzoqYolgaAvtomobileKerak || avtomobilegaYonolgiKerak);
+const 
